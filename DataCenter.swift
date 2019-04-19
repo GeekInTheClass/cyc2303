08d831bf8d5221ea51674dfb61d042576130e312
cyc2303 <cyc0723@naver.com>
2019-04-19 17:06:52 +0900

    new

---
+++ DataCenter.swift
@@ -0,0 +1,34 @@
+//
+//  DataCenter.swift
+//  Yechaniiii
+//
+//  Created by Choi Yechan on 18/04/2019.
+//  Copyright © 2019 Choi Yechan. All rights reserved.
+//
+
+import Foundation
+
+struct Date{
+    var toyear :Int
+    var tomonth : Int
+    var today : Int
+    
+    var name: String
+    
+    var fromyear :Int
+    var frommonth : Int
+    var fromday : Int
+    
+    init(_ name : String, _ y1:Int, _ m1:Int, _ d1:Int, _ y2:Int, _ m2:Int, _ d2:Int){
+        self.name=name
+        self.fromyear=y1
+        self.frommonth=m1
+        self.fromday=d1
+        self.toyear=y2
+        self.tomonth=m2
+        self.today=d2
+    }
+}
+
+
+var DateArray : Array<Date> = [Date("date1", 2018,08,03,2018,09,04),Date("date2", 2018,08,03,2018,09,04),Date("date3", 2018,08,03,2018,09,04),Date("date4", 2018,08,03,2018,09,04),Date("date5", 2018,08,03,2018,09,04),Date("date6", 2018,08,03,2018,09,04),Date("date7", 2018,08,03,2018,09,04),Date("date8", 2018,08,03,2018,09,04),Date("date9", 2018,08,03,2018,09,04),Date("date10", 2018,08,03,2018,09,04)]
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yechaniiii
+//
+//  Created by Choi Yechan on 18/04/2019.
+//  Copyright © 2019 Choi Yechan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
